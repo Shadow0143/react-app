5dfad8c110bbf82dad6aa86ebd4ca80c2d3cc8c9
shadow0143 <belal8804505973s@gmail.com>
2023-11-09 22:58:24 +0530

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,34 @@
 function ListGroup() {
-  const item = ["Shakil", "Dilshad", "Nishar", "John Doe", "Super Man"];
+  let item = ["Shakil", "Dilshad", "Nishar", "John Doe", "Super Man"];
+  item = [];
 
+  //Method 1
+  //   if (item.length === 0)
+  //     return (
+  //       <>
+  //         <h1>List</h1>
+  //         <p>No Data Found.</p>
+  //       </>
+  //     );
+
+  //Method 3
+  //   const message = item.length === 0 ? <p> No data found. </p> : null;
+
+  //   function getMessage() {
+  //     return item.length === 0 ? <p> No data found. </p> : null;
+  //   }
   return (
     <>
       <h1>List</h1>
+      {/* Method 2 Use Ternari operator instead of method 1 */}
+      {/* {item.length === 0 ? <p> No data found. </p> : null} */}
+
+      {/* Method 3 */}
+      {/* {getMessage()} */}
+
+      {/* Method 4  => always true && 'shakil' will gives shakil but false && 'Ahmad' gives false*/}
+      {item.length === 0 && <p> No data found. </p>}
+
       <ul className="list-group">
         {item.map((item) => (
           <li className="list-group-item" key={item}>
